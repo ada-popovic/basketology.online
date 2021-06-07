fbff05e0d7cfb88196904602a753ee3834988c3d
ada popovic <popowucz.adrianna@gmail.com>
2021-06-07 09:46:34 +0200

    video bacground, blur turnaround

--- script.js
+++ script.js
@@ -1,114 +1,56 @@
-/* HOME FILTERS */
+// $(document).ready(function() {
+//
+//   $('.turnaround').hover(
+//       function (event) {
+//           var elem = document.elementFromPoint(event.clientX, event.clientY);
+//           $('.background-video').addClass('.blur');
+//       },
+//       function (event) {
+//           $('.background-video').removeClass('.blur');
+//
+//       }
+//   );
+//
+//
+//
+// });
 
-$(function() {
 
-  $('.item-container').addClass('inactive');
+//
+// $('turnaround').hover(function(){
+//     $('turnaround').addClass('blur'); // mouse over
+// },
+// function(){
+//     $('turnaround').removeClass('blur'); // mouse out
+// });
 
-  function checkChecked(){
-    $("#filters :checkbox:checked").each(function() {
-      $("." + $(this).val()).removeClass('inactive');
-    });
-  }
+// $(document).ready(function() {
+//
+//   $(function() {
+//     $('#turnaround').hover(function() {
+//       $('#myVideo').css('-webkit-filter', 'blur(10px)');
+//     }, function() {
+//       // on mouseout, reset the background colour
+//       $('#myVideo').css('-webkit-filter', 'blur(0px)');
+//     });
+//   });
+//
+//
+//
+//
+//
+//
+//
+//
+// });
 
-  checkChecked();
+// $(document).ready(function() {
+//     $("turnaround").hover(function () {
+//         $("background-video").addClass('blur');
+//     };
 
-  $("#filters :checkbox").click(function() {
-    $(".item-container").addClass('inactive');
-    checkChecked();
-  });
+    // $("turnaround").click(function() {
+    //     $("background-video").removeClass('.blur');
+    // });
 
-});
-
-/* AJAX */
-
-$(function() {
-
-  function menuActive() { // Function to toggle active menu links
-      $('.menu a').removeClass('is-active'); // Remove "active" class on menu links after an ajax call
-      var pgurl = window.location.href; // Get the page url
-  // var pguri = window.location.href.substr(window.location.href.lastIndexOf("/") + 1);
-  // "uri" gets only the link folder while "url" gets the entire link
-  // Left here in case anyone out there uses $p->uri() on the menu
-      $(".menu a").each(function () {
-          if ($(this).attr("href") == pgurl || $(this).attr("href") == '') { // Compare url to links
-              $(this).addClass("is-active"); // Set "active" class on menu links
-              $(this).parents('li').children('a').addClass('is-active'); // Set "active" class on the parent of submenu links
-          }
-      });
-  }
-
-  // Requires jQuery 1.7+ to utilize the new "on" event attachment
-  $("body").on("click", "a", function (event) {
-      $.pjax({
-          "url": $(this).attr("href"),
-          "fragment": "#pjax-container",
-          "container": "#pjax-container",
-          "complete": function (data) { // Want to do something else? Left here for reasons
-          }
-      });
-      event.preventDefault();
-  });
-
-  $(document).on('pjax:start', function () {
-      NProgress.start(); // Start the nprogress bar
-  });
-  $(document).on('pjax:end', function () {
-      NProgress.done(); // End the nprogress bar
-      menuActive(); // Update the "active" class on links after ajax call
-      checkIfLightbox();
-      setTimeout(fadeLightbox, 100);
-  });
-  $(document).ready(menuActive); // Update the "active" class on links on first load
-
-  /* LIGHTBOX POSITIONING AND LINKING */
-
-  // Laurel's scroll stuff, lightbox positioning, lightbox linking
-
-  $.pjax.defaults.scrollTo = false;
-
-  var tempScrollTop = $(window).scrollTop();
-
-  $(window).scroll(function (event) {
-      tempScrollTop = $(window).scrollTop();
-  });
-
-  function checkIfLightbox(){
-
-    if ($('.menu .is-active').length){
-      $('a.background-lightbox').show();
-      $('#can-be-fixed').addClass('fixed');
-      // $('#pjax-container').css('top', tempScrollTop);
-      $('#pjax-container').addClass('scroll');
-      // $('body').css('overflow', 'hidden');
-    }
-
-    else {
-      $('a.background-lightbox').hide();
-      $('#can-be-fixed').removeClass('fixed');
-      $('#pjax-container').removeClass('scroll');
-      // $('body').css('overflow', 'scroll');
-    }
-
-  }
-
-  /* SCROLL TO TOP */
-
-  $('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 1000);
-    return false;
-  });
-
-  /* FADE IN BLURRY STUFF */
-
-  setTimeout(function(){
-    $('nav.menu').css('opacity', '1');
-    $('.publication-container, .event-container').css('opacity', '1');
-  }, 250);
-
-  function fadeLightbox(){
-    $('.publication-container, .event-container').css('opacity', '1');
-  }
-
-});
+// });
