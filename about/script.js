71ec85c72b48790587ccefafade7ef315af5dd6a
ada popovic <popowucz.adrianna@gmail.com>
2021-06-09 09:58:07 +0200

    added nav

--- about/script.js
+++ about/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
-    animateDiv('.story-1');
-    animateDiv('.story-2');
-    animateDiv('.story-3');
-    animateDiv('.story-4');
+  animateDiv('.story-1');
+  animateDiv('.story-2');
+  animateDiv('.story-3');
+  animateDiv('.story-4');
 
 });
 
 function makeNewPosition(){
 
     // Get viewport dimensions (remove the dimension of the div)
-    var h = $(window).height() - 50;
-    var w = $(window).width() - 50;
+    var h = $('#stories-container').height() - 50;
+    var w = $('#stories-container').width() - 50;
 
     var nh = Math.floor(Math.random() * h);
     var nw = Math.floor(Math.random() * w);
@@ -21,8 +21,25 @@ function makeNewPosition(){
 
 function animateDiv(myclass){
     var newq = makeNewPosition();
-    $(myclass).animate({ top: newq[0], left: newq[1] }, 10000,   function(){
+    var oldq = $(myclass).offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+    $(myclass).animate({ top: newq[0], left: newq[1] }, speed,   function(){
       animateDiv(myclass);
     });
 
 };
+
+function calcSpeed(prev, next) {
+
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+
+    var greatest = x > y ? x : y;
+
+    var speedModifier = 0.1;
+
+    var speed = Math.ceil(greatest/speedModifier);
+
+    return speed;
+
+}
