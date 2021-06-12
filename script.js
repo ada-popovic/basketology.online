a2bec1e8896c9c5fd2a6bf0389deba2932cf9c34
ada popovic <popowucz.adrianna@gmail.com>
2021-06-12 21:33:17 +0200

    audio player tests

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
   // ANIMATE FLOATING STORIES
 
   animateDiv('.story-1');
-  // animateDiv('.story-2');
-  // animateDiv('.story-3');
-  // animateDiv('.story-4');
+  animateDiv('.story-2');
+  animateDiv('.story-3');
+  animateDiv('.story-4');
 
   // fixing the touch event
   var eventname = 'click';
@@ -230,6 +230,198 @@ $('.accordion').on(eventname, function(event) {
 
 });
 
+
+// AUDIO PLAYER -1
+
+// var canvasWidth = 500
+// var audioEl = document.getElementById("audio")
+// var canvas = document.getElementById("progress").getContext('2d')
+// var ctrl = document.getElementById('audioControl')
+//
+// audioEl.addEventListener('loadedmetadata', function() {
+//   var duration = audioEl.duration
+//   var currentTime = audioEl.currentTime
+//   document.getElementById("duration").innerHTML = convertElapsedTime(duration)
+//   document.getElementById("current-time").innerHTML = convertElapsedTime(currentTime)
+//   canvas.fillRect(0, 0, canvasWidth, 50);
+// });
+//
+// function togglePlaying() {
+//
+//   var play = ctrl.innerHTML === 'Play'
+//   var method
+//
+//   if (play) {
+//     ctrl.innerHTML = 'Pause'
+//     method = 'play'
+//   } else {
+//     ctrl.innerHTML = 'Play'
+//     method = 'pause'
+//   }
+//
+//   audioEl[method]()
+//
+// }
+//
+// function updateBar() {
+//   canvas.clearRect(0, 0, canvasWidth, 50)
+//   canvas.fillStyle = "white";
+//   canvas.fillRect(0, 0, canvasWidth, 50)
+//
+//   var currentTime = audioEl.currentTime
+//   var duration = audioEl.duration
+//
+//   if (currentTime === duration) {
+//     ctrl.innerHTML = "Play"
+//   }
+//
+//   document.getElementById("current-time").innerHTML = convertElapsedTime(currentTime)
+//
+//   var percentage = currentTime / duration
+//   var progress = (canvasWidth * percentage)
+//   canvas.fillStyle = "lime"
+//   canvas.fillRect(0, 0, progress, 50)
+// }
+//
+// function convertElapsedTime(inputSeconds) {
+//   var seconds = Math.floor(inputSeconds % 60)
+//   if (seconds < 10) {
+//     seconds = "0" + seconds
+//   }
+//   var minutes = Math.floor(inputSeconds / 60)
+//   return minutes + ":" + seconds
+// }
+
+// AUDIO PLAYER -2
+
+// var timer;
+// var percent = 0;
+// var audio = document.getElementById("audio");
+// audio.addEventListener("playing", function(_event) {
+//   var duration = _event.target.duration;
+//   advance(duration, audio);
+// });
+// audio.addEventListener("pause", function(_event) {
+//   clearTimeout(timer);
+// });
+// var advance = function(duration, element) {
+//   var progress = document.getElementById("progress");
+//   increment = 10/duration
+//   percent = Math.min(increment * element.currentTime * 10, 100);
+//   progress.style.width = percent+'%'
+//   startTimer(duration, element);
+// }
+// var startTimer = function(duration, element){
+//   if(percent < 100) {
+//     timer = setTimeout(function (){advance(duration, element)}, 100);
+//   }
+// }
+//
+// function togglePlay (e) {
+//   e = e || window.event;
+//   var btn = e.target;
+//   if (!audio.paused) {
+//     btn.classList.remove('active');
+//     audio.pause();
+//     isPlaying = false;
+//   } else {
+//     btn.classList.add('active');
+//     audio.play();
+//     isPlaying = true;
+//   }
+// }
+
+// AUDIO PLAYER -3
+
+//
+// const audioPlayer = document.querySelector(".audio-player");
+// const audio = new Audio(
+//   "https://ia800905.us.archive.org/19/items/FREE_background_music_dhalius/backsound.mp3"
+// );
+// //credit for song: Adrian kreativaweb@gmail.com
+//
+// console.dir(audio);
+//
+// audio.addEventListener(
+//   "loadeddata",
+//   () => {
+//     audioPlayer.querySelector(".time .length").textContent = getTimeCodeFromNum(
+//       audio.duration
+//     );
+//     audio.volume = .75;
+//   },
+//   false
+// );
+//
+// //click on timeline to skip around
+// const timeline = audioPlayer.querySelector(".timeline");
+// timeline.addEventListener("click", e => {
+//   const timelineWidth = window.getComputedStyle(timeline).width;
+//   const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
+//   audio.currentTime = timeToSeek;
+// }, false);
+//
+// //click volume slider to change volume
+// const volumeSlider = audioPlayer.querySelector(".controls .volume-slider");
+// volumeSlider.addEventListener('click', e => {
+//   const sliderWidth = window.getComputedStyle(volumeSlider).width;
+//   const newVolume = e.offsetX / parseInt(sliderWidth);
+//   audio.volume = newVolume;
+//   audioPlayer.querySelector(".controls .volume-percentage").style.width = newVolume * 100 + '%';
+// }, false)
+//
+// //check audio percentage and update time accordingly
+// setInterval(() => {
+//   const progressBar = audioPlayer.querySelector(".progress");
+//   progressBar.style.width = audio.currentTime / audio.duration * 100 + "%";
+//   audioPlayer.querySelector(".time .current").textContent = getTimeCodeFromNum(
+//     audio.currentTime
+//   );
+// }, 500);
+//
+// //toggle between playing and pausing on button click
+// const playBtn = audioPlayer.querySelector(".controls .toggle-play");
+// playBtn.addEventListener(
+//   "click",
+//   () => {
+//     if (audio.paused) {
+//       playBtn.classList.remove("play");
+//       playBtn.classList.add("pause");
+//       audio.play();
+//     } else {
+//       playBtn.classList.remove("pause");
+//       playBtn.classList.add("play");
+//       audio.pause();
+//     }
+//   },
+//   false
+// );
+//
+// audioPlayer.querySelector(".volume-button").addEventListener("click", () => {
+//   const volumeEl = audioPlayer.querySelector(".volume-container .volume");
+//   audio.muted = !audio.muted;
+//   if (audio.muted) {
+//     volumeEl.classList.remove("icono-volumeMedium");
+//     volumeEl.classList.add("icono-volumeMute");
+//   } else {
+//     volumeEl.classList.add("icono-volumeMedium");
+//     volumeEl.classList.remove("icono-volumeMute");
+//   }
+// });
+//
+// //turn 128 seconds into 2:08
+// function getTimeCodeFromNum(num) {
+//   let seconds = parseInt(num);
+//   let minutes = parseInt(seconds / 60);
+//   seconds -= minutes * 60;
+//   const hours = parseInt(minutes / 60);
+//   minutes -= hours * 60;
+//
+//   if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+//   return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+//     seconds % 60
+//   ).padStart(2, 0)}`;
+// }
 
 
 
